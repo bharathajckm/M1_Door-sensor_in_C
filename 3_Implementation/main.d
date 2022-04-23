26f27cb7b1c629bdc1ef04b5d31384bbda5358c2
BHARATH A J <93699185+bharathajckm@users.noreply.github.com>
2022-04-23 13:33:56 +0530

    Add files via upload

---
+++ 3_Implementation/main.d
@@ -0,0 +1,37 @@
+main.d main.o: .././main.c \
+ d:\aj\ phone\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ d:\aj\ phone\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ d:\aj\ phone\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ d:\aj\ phone\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ d:\aj\ phone\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ D:\AJ\ phone\7.0\Packs\atmel\ATmega_DFP\1.6.364\include/avr/iom328.h \
+ D:\AJ\ phone\7.0\Packs\atmel\ATmega_DFP\1.6.364\include/avr/iom328p.h \
+ d:\aj\ phone\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ d:\aj\ phone\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ d:\aj\ phone\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ d:\aj\ phone\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ d:\aj\ phone\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h
+
+d:\aj\ phone\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+d:\aj\ phone\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+d:\aj\ phone\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+d:\aj\ phone\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+d:\aj\ phone\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+D:\AJ\ phone\7.0\Packs\atmel\ATmega_DFP\1.6.364\include/avr/iom328.h:
+
+D:\AJ\ phone\7.0\Packs\atmel\ATmega_DFP\1.6.364\include/avr/iom328p.h:
+
+d:\aj\ phone\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+d:\aj\ phone\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+d:\aj\ phone\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+d:\aj\ phone\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+d:\aj\ phone\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
